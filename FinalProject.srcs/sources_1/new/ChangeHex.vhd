61fd5bd7052aaa257dbf2eff9840e58cdfaea5c5
Chad Ross <cross20@my.whitworth.edu>
2019-11-25 10:16:04 -0800

    HexMod and ChangeHex

--- FinalProject.srcs/sources_1/new/ChangeHex.vhd
+++ FinalProject.srcs/sources_1/new/ChangeHex.vhd
@@ -2,12 +2,37 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity ChangeHex is
-    --Port ();
+    Port (  CLK : in STD_LOGIC;
+            SW : in STD_LOGIC_VECTOR(15 downto 0);
+            AN : out STD_LOGIC_VECTOR(7 downto 0);
+            CT : out STD_LOGIC_VECTOR(6 downto 0));
 end ChangeHex;
 
 architecture ChangeHex of ChangeHex is
 
+component HexMod is
+    Port (  SW : in STD_LOGIC_VECTOR(15 downto 0);
+            AN : out STD_LOGIC_VECTOR(7 downto 0);
+            CT : out STD_LOGIC_VECTOR(6 downto 0));
+end component;
+
+component ClockMod is
+    Port (
+        CLK100MHZ : in std_logic;
+        C0 : out std_logic;
+        C1 : out std_logic;
+        C2 : out std_logic;
+        C3 : out std_logic
+        );
+end component;
+
+signal C0, C1, C2, C3: STD_LOGIC;
+
 begin
+
+H1 : HexMod port map(SW=>SW, AN=>AN, CT=>CT);
+Clock : ClockMod port map(CLK100MHZ=>CLK, C0=>C0, C1=>C1, C2=>C2, C3=>C3);
+
 
 
 end ChangeHex;
--- FinalProject.srcs/sources_1/new/HexMod.vhd
+++ FinalProject.srcs/sources_1/new/HexMod.vhd
@@ -3,12 +3,26 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 
 entity HexMod is
-    -- Port ( );
+    Port (  SW : in STD_LOGIC_VECTOR(15 downto 0);
+            AN : out STD_LOGIC_VECTOR(7 downto 0);
+            CT : out STD_LOGIC_VECTOR(6 downto 0));
 end HexMod;
 
 architecture HexMod of HexMod is
 
 begin
 
+process(SW(2 downto 1))
+begin
+    if SW(2 downto 1) = "01" then
+        AN(7 downto 0) <= "11111000";
+    elsif SW(2 downto 1) = "10" then
+        AN(7 downto 0) <= "11110000";
+    else
+        AN(7 downto 0) <= "11111111";
+    end if;
+end process;
+
+CT(6 downto 0) <= "0000000"; -- Make 8's.
 
 end HexMod;
